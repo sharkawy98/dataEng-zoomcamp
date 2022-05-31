b951ea76462f8553df3636fc2ca7a237ad9f2e2c
Ahmed Elsharkawy <sharkawyy98@gmail.com>
2022-05-31 03:39:52 +0200

    Fix <docker build> error with postgres container ny_taxi_data folder

--- week_1_docker_n_terraform/1_docker/pg_script.sh
+++ week_1_docker_n_terraform/1_docker/pg_script.sh
@@ -5,7 +5,7 @@ docker run -it \
     -e POSTGRES_USER="root" \
     -e POSTGRES_PASSWORD="root" \
     -e POSTGRES_DB="ny_taxi" \
-    -v $(pwd)/ny_taxi_data:/var/lib/postgresql/data \
+    -v $(pwd)/data/ny_taxi_data:/var/lib/postgresql/data \
     -p 5432:5432 \
     --network=pg-network \
     --name=pg-database \
